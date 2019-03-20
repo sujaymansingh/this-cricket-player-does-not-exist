6a2eaaec6e4e8602fa35351a97a4767ebd1107c6
Sujay Mansingh <sujay.mansingh@gmail.com>
2019-03-20 16:41:09 +0000

    include player name in the url
    It makes the url nicer

--- player_generator/api.py
+++ player_generator/api.py
@@ -5,6 +5,7 @@ from glob import glob
 from os import getenv
 
 from flask import Flask, redirect, render_template, url_for
+from slugify import slugify
 
 from . import load_players_from_file, PlayerGenerator
 from .countries import get_all_countries, get_country_by_slug
@@ -21,26 +22,40 @@ def get_latest_filename():
 
 with open(get_latest_filename(), "r") as input_file:
     players = load_players_from_file(input_file)
-    generator = PlayerGenerator(players, min_profile_length=100)
+    generator = PlayerGenerator(players, min_profile_length=150)
 
 
-@app.route("/p/<country_slug>/<seed_str>")
-def view_player(country_slug, seed_str):
+def generate_url(country, seed, player):
+    return url_for(
+        "view_player",
+        country_slug=country.slug,
+        seed_str=encode(seed),
+        name_slug=slugify(player.fullname),
+    )
+
+
+@app.route("/p/<country_slug>/<seed_str>/<name_slug>")
+def view_player(country_slug, seed_str, name_slug):
     country = get_country_by_slug(country_slug)
     seed = decode(seed_str)
 
     (player, _seed) = generator.generate(country_id=country.country_id, seed=seed)
+
+    if name_slug != slugify(player.fullname):
+        # What's happened here?! Something odd. I'm torn between a 404 or a redirect.
+        # A redirect is kinder to the user.
+        return redirect(generate_url(country, seed, player))
+
     return render_template("player.html", player=player)
 
 
 @app.route("/random")
 def random_player():
     country = random.choice(get_all_countries())
-
     seed = random.randint(1_000_000, 999_999_999)
-    seed_str = encode(seed)
+    (player, _seed) = generator.generate(country_id=country.country_id, seed=seed)
 
-    url = url_for("view_player", country_slug=country.slug, seed_str=seed_str)
+    url = generate_url(country, seed, player)
     return redirect(url)
 
 
